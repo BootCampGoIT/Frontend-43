4c814c9a1dd40c92e8f27a384066c4fb6b2bab16
AndriiShnurkov <Shans12.84@ukr.net>
2021-04-27 22:00:19 +0300

    switched branch

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./header/Header";
 import Main from "./main/Main";
-import Navigation from "./navigation/Navigation";
 
 const App = () => {
   return (
